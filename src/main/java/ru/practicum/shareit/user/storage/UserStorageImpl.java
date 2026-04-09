1cc8506b06c3fb6d46cd7d2d14840e926d50c683
dmitry-udovin <dimav0406@gmail.com>
2026-04-09 16:11:22 +0300

    fix: checkstyle

--- src/main/java/ru/practicum/shareit/user/storage/UserStorageImpl.java
+++ src/main/java/ru/practicum/shareit/user/storage/UserStorageImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.user.storage;
 
 import org.springframework.stereotype.Component;
-import ru.practicum.shareit.user.exception.EmailAlreadyUserException;
 import ru.practicum.shareit.user.exception.UserNotFoundException;
 import ru.practicum.shareit.user.model.User;
 
