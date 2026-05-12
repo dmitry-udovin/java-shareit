6f212e2beb0e8de28d32025bc956ae6e8adee407
dmitry-udovin <dimav0406@gmail.com>
2026-05-12 19:34:09 +0300

    fix(gateway): docker-профиль, URL сервера и ускорение старта для CI

--- shareit-gateway/src/main/java/ru/practicum/shareit/gateway/ShareItGatewayApp.java
+++ shareit-gateway/src/main/java/ru/practicum/shareit/gateway/ShareItGatewayApp.java
@@ -3,10 +3,17 @@ package ru.practicum.shareit.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @SpringBootApplication(scanBasePackages = "ru.practicum.shareit.gateway")
 public class ShareItGatewayApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShareItGatewayApp.class, args);
+        SpringApplication app = new SpringApplication(ShareItGatewayApp.class);
+        if (Files.exists(Path.of("/.dockerenv"))) {
+            app.setAdditionalProfiles("docker");
+        }
+        app.run(args);
     }
 }
