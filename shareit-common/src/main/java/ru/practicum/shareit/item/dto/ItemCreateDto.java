faf27e39914008d2554698d618a023fcefc8cde3
dmitry-udovin <dimav0406@gmail.com>
2026-05-13 14:11:32 +0300

    fix: ItemCreateDto (@NotNull + Boolean)

--- shareit-common/src/main/java/ru/practicum/shareit/item/dto/ItemCreateDto.java
+++ shareit-common/src/main/java/ru/practicum/shareit/item/dto/ItemCreateDto.java
@@ -1,10 +1,11 @@
 package ru.practicum.shareit.item.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ItemCreateDto(@NotBlank(message = "Имя не может быть пустым") String name,
                             @NotBlank(message = "описание вещи не может оставаться пустым") String description,
-                            boolean available,
+                            @NotNull(message = "Доступность вещи должна быть указана") Boolean available,
                             Long requestId) {
 
 }
